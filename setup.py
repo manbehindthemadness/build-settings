53d1c6fcf8479e595448afcd37e8285cf6134aaa
Kev0 <kev0@me.com>
2024-03-23 11:42:36 -0400

    Revised version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="build-settings",
-    version="0.1",
+    version="0.1.1",
     author="Manbehindthemadness",
     author_email="manbehindthemadness@gmail.com",
     description="An extension of configparser",
